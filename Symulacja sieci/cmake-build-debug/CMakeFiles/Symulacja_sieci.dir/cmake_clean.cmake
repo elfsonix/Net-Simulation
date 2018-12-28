06383cc43660350a9d4b6e7183ba1207f3e4720e
Sonia Wittek <39004441+elfsonix@users.noreply.github.com>
2018-12-28 14:59:22 +0100

    Utworzenie folderu Symulacja sieci
    W środku bazowy prjekt na clion

---
+++ Symulacja sieci/cmake-build-debug/CMakeFiles/Symulacja_sieci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Symulacja_sieci.dir/main.cpp.obj"
+  "Symulacja_sieci.pdb"
+  "Symulacja_sieci.exe"
+  "Symulacja_sieci.exe.manifest"
+  "libSymulacja_sieci.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Symulacja_sieci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Symulacja sieci/cmake-build-debug/CMakeFiles/Symulacja_sieci.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Symulacja_sieci.
+# This may be replaced when dependencies are built.
