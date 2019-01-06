cea970a32c8fae8e96bd07ef02bff6f016bb6a14
Sonia Wittek <sonia@wittek.pl>
2019-01-06 23:56:41 +0100

    Revert "Revert "Wersja końcowa API""
    This reverts commit 8d9845465b30a2c02ff1817e6b07dc49af035981.

--- Symulacja sieci/include/Worker.hpp
+++ Symulacja sieci/include/Worker.hpp
@@ -9,6 +9,8 @@
 
 #include "Package.hpp"
 #include <memory>
+#include <deque>
+#include <tuple>
 
 class Worker : public PackageSender, public IPackageReceiver, public IPackageQueue {
 private:
@@ -18,11 +20,11 @@ public:
     Worker(std::unique_ptr<Package> package_queue, ElementID nodeId) : PackageSender(), IPackageReceiver(), IPackageQueue() {}
     void processPackage();
     void receivePackage(const Package& package);
-    tuple<> identifyReceiver() const;
+    std::tuple<ReceiverType, ElementID> identifyReceiver() const;
     std::deque<Package>::const_iterator  cbegin() const;
-    deque<Package>::const_iterator cend() const;
-    deque<Package>::iterator begin() const;
-    deque<Package>::iterator end() const;
+    std::deque<Package>::const_iterator cend() const;
+    std::deque<Package>::iterator begin() const;
+    std::deque<Package>::iterator end() const;
     Package popPackage();
     QueueType returnQueueType() const;
 
