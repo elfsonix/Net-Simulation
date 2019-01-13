303eb8f92ffe59aaca1bb93fb8d96c64f35b258a
Unknown <katarzyna.watorska@gmail.com>
2019-01-14 00:00:37 +0100

    Działająca pierwsza faza implementacji

--- Symulacja sieci/test/main_test.cpp
+++ Symulacja sieci/test/main_test.cpp
@@ -1,9 +1,11 @@
-//
-// Created by Kasia on 2018-12-31.
-//
+// 4b_4: Wittek (297473), Wątorska (297469), Rabajczyk (286498)
+// Created by Katarzyna Wątorska
+
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+// 4b_4: Wittek (297473), Wątorska (297469), Rabajczyk (286498)
+
